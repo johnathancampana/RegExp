325a94090315aadd1a7682d2f72fb1239811f7cc
Johnathan Campana <johnathancampana@Johnathans-Air.home>
2020-03-18 13:57:59 -0400

    cleaned up highlight

--- regex.js
+++ regex.js
@@ -5,23 +5,18 @@
 
 // console.log(highlight(/el/g, "hello"));
 
-const regexp = RegExp('hi[a-z]*','g');
-const str = 'hitter, him, howdy';
+const r = RegExp('hi[a-z]*','g');
+const s = 'hitter, thimble, howdy';
 let match;
 
-function highlight(regex, s) {
-    let matches = s.matchAll(regex);
-    console.log(matches);
+function highlight(regexp, str) {
+
+    let matches = str.matchAll(regexp);
+
     for (const match of matches) {
-        let m = match[0];
-        let start = match.index;
-        let end = match.index + match[0].length;
+        let m = match[0], start = match.index, end = match.index + match[0].length, inp = match.input;
         console.log(`Found ${m} start=${start} end=${end}.`);
-        var result = document.getElementById("result");
-        let text = document.createTextNode(''+intValue);
-        result.innerHTML = '';
-        result.append(text);
     }
 }
 
-highlight(regexp, str);
+highlight(r, s);

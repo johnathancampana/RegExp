6f3f94d8664fd7f0b483f9334e5939d7d894b2dd
Johnathan Campana <johnathancampana@Johnathans-Air.home>
2020-03-18 18:46:05 -0400

    cleaned up highlight and added highlight functionality to html

--- regex.js
+++ regex.js
@@ -1,42 +1,34 @@
-const r = 'hi[a-z]*';
-const s = 'hitter, thimble, howdy';
-let match;
-
 function highlight(regexp, str) {
 
     let reg = RegExp(regexp, 'g');
-    let matches = str.matchAll(reg);
+    let matches = str.matchAll(regexp);
     let arr = [0];
 
     for (const match of matches) {
         let m = match[0], start = match.index, end = match.index + match[0].length;
         arr.push(start, end);
-        console.log(`Found ${m} start=${start} end=${end}.`);
-        // str = str.substr(0, start) + '<span class="highlight">' + str.substr(start, end - start + 1) + 
-        // '</span>' + str.substr(end + 1);
     }
 
     let i = 0;
     let ret = '';
     let inHighlighted = false;
+
     while (i < str.length) {
         if (arr.includes(i)) {
             if (inHighlighted) {
                 ret += '</span>';
             }
             else {
-                ret += '<span class="highlight">'
-
+                ret += '<span class="highlight">';
             }
-            inHighlighted = !inHighlighted
+            inHighlighted = !inHighlighted;
         }
         ret += str.charAt(i);
         i++;
     }
-    var element = document.getElementById("result");
-    element.innerHTML = ret;
-    console.log('return =', ret);
+    if (inHighlighted) {
+        ret += '</span>';
+    }
+    
     return ret;
 }
-
-highlight(r, s);
